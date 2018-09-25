dd661f413fcdccdf5a07de2f5c92c8d99cd28dd3
oromanchenko <o.romanchenko@levi9.com>
2018-09-25 13:18:18 +0300

    API-546 pass jshint check

--- src/js/modules/loader.js
+++ src/js/modules/loader.js
@@ -7,23 +7,23 @@ var Core = (function (Core, settings) {
 
     var show = function (text) {
         domElement.setAttribute("data-text", (text || loadingText));
-        domElement.classList.add('Loader--IsActive');
+        domElement.classList.add("Loader--IsActive");
     };
 
     var hide = function () {
-        domElement.classList.remove('Loader--IsActive');
+        domElement.classList.remove("Loader--IsActive");
     };
 
     var setup = function () {
-        var $loader = document.getElementById('Loader');
+        var $loader = document.getElementById("Loader");
 
         if ($loader) {
             domElement = $loader;
         } else {
-            var newLoaderElement = document.createElement('div');
-            newLoaderElement.classList.add('Loader');
-            newLoaderElement.setAttribute('id', 'Loader');
-            newLoaderElement.setAttribute('data-text', loadingText);
+            var newLoaderElement = document.createElement("div");
+            newLoaderElement.classList.add("Loader");
+            newLoaderElement.setAttribute("id", "Loader");
+            newLoaderElement.setAttribute("data-text", loadingText);
             document.body.appendChild(newLoaderElement);
 
             domElement = newLoaderElement;
